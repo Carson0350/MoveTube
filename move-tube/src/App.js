30812ff5433aeb496f4a088e5c97102fd83bcd35
Carson0350 <carson.workflow@gmail.com>
2021-01-14 12:27:31 -0700

    test

---
+++ move-tube/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <h1>MoveTube</h1>
+    )
+  }
+}
+
+export default App
---
+++ move-tube/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+import App from './App'
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
