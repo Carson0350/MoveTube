b4fea4700756902bbf5ba4a4c4cf2edb1c5541cf
Carson0350 <carson.workflow@gmail.com>
2021-01-14 13:19:43 -0700

    layout and api got

--- move-tube/src/App.js
+++ move-tube/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoList, VideoDetail} from './components'
+
+import youtube from './api/youtube'
+
+
 class App extends React.Component {
   render () {
     return (
-      <h1>MoveTube</h1>
+      <Grid justify='center' container spaceing={16}>
+        <Grid item xs={12}>
+          <Grid container spaceing={16}>
+            <Grid item xs={12}>
+              {/* SEARCH BAR*/}
+            </Grid>
+            <Grid item xs={8}>
+              {/* Video details */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video List */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
